9368f662bc343835029971a4a6767a59217f7f75
NikoTico <52984591+nikotico@users.noreply.github.com>
2021-11-25 00:12:44 -0600

    fit white

--- Ventana.py
+++ Ventana.py
@@ -3,6 +3,7 @@ from population import *
 from functions import *
 from laberintos import *
 from tkinter import *
+from PIL import Image, ImageTk
 
 
 
@@ -12,6 +13,7 @@ from tkinter import *
 def load_img():
     # Get laberintos de prueba
     l = laberintos("IMGs",2)
+    """
     for i in range(10):
         l.evolve()
 
@@ -20,9 +22,23 @@ def load_img():
     for i in range(l.length()):
         l.print(i)
         input()
+    """
 
+def pruebaImg():
 
+    LabImg= (Image.open("\\IMGs\\1.png"))
+    #imgLabP= (ImageTk.PhotoImage(file = "\\IMGs\\1.png"))
 
+    #Resize the Image using resize method
+    resized_image= LabImg.resize((300,300), Image.ANTIALIAS)
+    new_image= ImageTk.PhotoImage(resized_image)
+
+    #Add image to the Canvas Items
+    can_inicial.create_image(0,0, anchor=NW, image=new_image)
+    can_inicial.new_image = new_image
+
+    #canvasLabFinP.create_image(0,0, anchor=NW, image = imgLabP)
+    #canvasLabFinP.imgLabP = imgLabP
 
 
 
@@ -54,10 +70,11 @@ canvas.create_text(60, 20, fill="black", font="Verdana 15", text="Inicial")
 can_inicial = Canvas(gui, bg='#c4c4fc', width = w, height = h)
 can_inicial.place(x = 20, y = 125)
 
+'''
 txt_inicial = Text(gui, height= 15,width= 50)
 txt_inicial.pack()
 txt_inicial.place(x = 20, y = 550)
-
+'''
 
 
 
@@ -66,10 +83,11 @@ canvas.create_text(500, 20, fill="black", font="Verdana 15", text="Anterior")
 can_anterior = Canvas(gui, bg='#678dce', width = w, height = h)
 can_anterior.place(x = 450, y = 125)
 
+'''
 txt_anterior = Text(gui, height= 15,width= 50)
 txt_anterior.pack()
 txt_anterior.place(x = 450, y = 550)
-
+'''
 
 
 
@@ -78,10 +96,11 @@ canvas.create_text(930, 20, fill="black", font="Verdana 15", text="Actual")
 can_actual = Canvas(gui, bg='#9bccfc', width = w, height = h)
 can_actual.place(x = 880, y = 125)
 
+'''
 txt_actual = Text(gui, height= 15,width= 50)
 txt_actual.pack()
 txt_actual.place(x = 880, y = 550)
-
+'''
 
 
 
@@ -90,14 +109,18 @@ txt_actual.place(x = 880, y = 550)
 #Footer
 nombre = IntVar()
 nombre_img = Entry(gui, textvariable = nombre, width = "12", font = ("Arial", "12"), bg= "white")
-nombre_img.place(x = 20, y = 25)
+nombre_img.place(x = 200, y = 25)
 
-btn_load = Button(gui, text="Load", command = load_img(), width= "10", height= "1", bg= "#c0d6e4", font = "Arial 10", fg = "black")
-btn_load.place(x = 150, y = 25 )
+btn_load = Button(gui, text="Load", command = pruebaImg(), width= "10", height= "2", bg= "#3B527E", font = "Arial 15", fg = "black")
+btn_load.place(x = 150, y = 850 )
 
 
-btn_load = Button(gui, text="STEP", command = None, width= "30", height= "2", bg= "#ffa6bb", font = "Arial 20", fg = "white")
-btn_load.place(x = 400, y = 850 )
+btn_exe = Button(gui, text="Execute", command = None, width= "10", height= "2", bg= "#3B527E", font = "Arial 15", fg = "black")
+btn_exe.place(x = 400, y = 850 )
+
+btn_next = Button(gui, text="Next Img", command = load_img(), width= "10", height= "2", bg= "#3B527E", font = "Arial 15", fg = "black")
+btn_next.place(x = 650, y = 850 )
+
 
 
 
